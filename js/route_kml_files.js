872efdba9ee11cc6d91f18f5894d4fac8ed49c02
John Skufca <john.skufca@imulus.com>
2015-01-30 10:46:25 -0700

    Imulus Commit
    - Added leaflet-omnivore kml
      to geojson real time converter
    - Separate out css and js into own files
    - Add legend to hide/show markers
    - Create js files for kml paths, icons, & colors

---
+++ js/route_kml_files.js
@@ -0,0 +1,11 @@
+﻿var generalRouteObj = new MapRoute(
+    "kml-files/routes_general_1.kml",
+    "#F77F00");
+
+var climbRouteObj = new MapRoute(
+    "kml-files/routes_climb_1.kml",
+    "#F77F00");
+
+var rideRouteObj = new MapRoute(
+    "kml-files/routes_ride_1.kml",
+    "#F77F00");
