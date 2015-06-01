9b5675919c04f76faafc9f9e31dd01ae79efc3fd
Alex Davidow <adavidow@michaeljfox.org>
2015-06-01 12:34:50 -0400

    trying to get rid of the git merge lines

--- js/map_app.js
+++ js/map_app.js
@@ -86,7 +86,11 @@ var layerCelebrate = omnivore.kml(celebrationMarkers.kmlUrl)
 
             var markerDate;
             var markerUrl;
-
+            if (markerDescArray.length == 8) {
+                markerDescArray.splice(2,1);
+                markerDescArray.splice(3,1);
+                markerDescArray.splice(4,1);                
+            }
             if (markerDescArray.length == 4) {
                 markerDate = markerDescArray[0];
                 markerUrl = markerDescArray[1] + " " + markerDescArray[2] + " " + markerDescArray[3];
@@ -100,7 +104,7 @@ var layerCelebrate = omnivore.kml(celebrationMarkers.kmlUrl)
                 markerDescFinal += "<a class='viewClimbLink' target='_blank' href='" + markerHref + "'>Join Sam</a>";
 
                 layer.bindPopup(markerDescFinal, { closeButton: false, keepInView: true });
-            }
+            } 
             else {
                 console.log("Invalid KML placemark description: " + markerDesc);
             }
